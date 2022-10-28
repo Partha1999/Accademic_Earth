5a59955341784a295cc9be0c797070b8c8b56225
Partha1999 <impartha1999@gmail.com>
2022-10-28 16:10:07 +0600

    blog done

--- src/Component/Blog/Blog.js
+++ src/Component/Blog/Blog.js
@@ -5,33 +5,27 @@ const Blog = () => {
         <div>
             <div className="text-center mt-20 lg:mx-20 mx-3 ">
       <div className="border-2 p-5 shadow-lg mb-5 bg-slate-200">
-        <h1 className="mb-5 text-2xl font-bold text-slate-700">What is the purpose of React router?</h1>
+        <h1 className="mb-5 text-2xl font-bold text-slate-700">What is cors?</h1>
         <p>
-          Ans:React Router is a standard library for routing in React. It
-          enables the navigation among views of various components in a React
-          Application, allows changing the browser URL, and keeps the UI in sync
-          with the URL. Let us create a simple application to React to
-          understand how the React Router works
+          Ans:Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources.
         </p>
       </div>
       <div className="border-2 p-5 shadow-lg mb-5 bg-slate-200">
-        <h1 className=" text-2xl font-bold mb-5 text-slate-700">How does contest API work?</h1>
+        <h1 className=" text-2xl font-bold mb-5 text-slate-700"> Why are you using firebase? What other options do you have to implement authentication?</h1>
         <p>
-          Ans:The React Context API is a way for a React app to effectively
-          produce global variables that can be passed around. This is the
-          alternative to "prop drilling" or moving props from grandparent to
-          child to parent, and so on. Context is also touted as an easier,
-          lighter approach to state management using Redux.
+          Ans:Firebase Authentication provides backend services, easy-to-use SDKs, and ready-made UI libraries to authenticate users to your app. It supports authentication using passwords, phone numbers, popular federated identity providers like Google, Facebook and Twitter, and more.
         </p>
       </div>
       <div className="border-2 p-5 shadow-lg mb-5 bg-slate-200">
-        <h1 className="mb-5 text-2xl font-bold text-slate-700">What is useRef hook and how does it works?</h1>
+        <h1 className="mb-5 text-2xl font-bold text-slate-700">How does the private route work?</h1>
         <p>
-          Ans:The useRef is a hook that allows to directly create a reference to
-          the DOM element in the functional component.How does useRef hook work?
-          The useRef Hook allows you to persist values between renders. It can
-          be used to store a mutable value that does not cause a re-render when
-          updated. It can be used to access a DOM element directly.
+          Ans:The react private route component renders child components ( children ) if the user is logged in. If not logged in the user is redirected to the /login page with the return url passed in the location state property
+        </p>
+      </div>
+      <div className="border-2 p-5 shadow-lg mb-5 bg-slate-200">
+        <h1 className="mb-5 text-2xl font-bold text-slate-700">What is Node? How does Node work??</h1>
+        <p>
+          Ans:It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node. js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive.
         </p>
       </div>
     </div>
