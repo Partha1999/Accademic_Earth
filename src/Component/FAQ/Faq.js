63ed7fb43f7de2c62abc09469a4a0d1f7a0c2364
Partha1999 <impartha1999@gmail.com>
2022-10-28 16:25:27 +0600

    Faq done

--- src/Component/FAQ/Faq.js
+++ src/Component/FAQ/Faq.js
@@ -6,33 +6,21 @@ const Faq = () => {
             <div>
             <div className="text-center mt-20 lg:mx-20 mx-3 ">
       <div className="border-2 p-5 shadow-lg mb-5 bg-slate-200">
-        <h1 className="mb-5 text-2xl font-bold text-slate-700">What is the purpose of React router?</h1>
+        <h1 className="mb-5 text-2xl font-bold text-slate-700">For whom this course for?</h1>
         <p>
-          Ans:React Router is a standard library for routing in React. It
-          enables the navigation among views of various components in a React
-          Application, allows changing the browser URL, and keeps the UI in sync
-          with the URL. Let us create a simple application to React to
-          understand how the React Router works
+          Ans:Who wants to become a web developer. For those who want to learn web development from scratch and then get a job in a software company as a web developer. For those who don't know much about programming or web development
         </p>
       </div>
       <div className="border-2 p-5 shadow-lg mb-5 bg-slate-200">
-        <h1 className=" text-2xl font-bold mb-5 text-slate-700">How does contest API work?</h1>
+        <h1 className=" text-2xl font-bold mb-5 text-slate-700">What types of experience a student should need to start this course?</h1>
         <p>
-          Ans:The React Context API is a way for a React app to effectively
-          produce global variables that can be passed around. This is the
-          alternative to "prop drilling" or moving props from grandparent to
-          child to parent, and so on. Context is also touted as an easier,
-          lighter approach to state management using Redux.
+          Ans:Since we are teaching from scratch. So you don't need to know anything beforehand. But if you have time, you can do these three things.1. You need to keep some brain space to get the things in your head after watching the video. 2. Just joining the course will not make you an auto web developer. Must have a hard-working, stick-to-it mentality. 3. If you get stuck somewhere, you have to have the mentality of asking for help and the will to stick around.
         </p>
       </div>
       <div className="border-2 p-5 shadow-lg mb-5 bg-slate-200">
-        <h1 className="mb-5 text-2xl font-bold text-slate-700">What is useRef hook and how does it works?</h1>
+        <h1 className="mb-5 text-2xl font-bold text-slate-700">why this course is not free?</h1>
         <p>
-          Ans:The useRef is a hook that allows to directly create a reference to
-          the DOM element in the functional component.How does useRef hook work?
-          The useRef Hook allows you to persist values between renders. It can
-          be used to store a mutable value that does not cause a re-render when
-          updated. It can be used to access a DOM element directly.
+          Ans:This course is not just a video watching course. Because there are 30 professional developers behind this course. Will see your 11 assignments. Assignment marks plus feedback will be provided. There will also be seven web developers who will be ready to answer any of your questions within 24 hours. If there is a problem with your code, there will be a dedicated developer to solve it by screen sharing..
         </p>
       </div>
     </div>
